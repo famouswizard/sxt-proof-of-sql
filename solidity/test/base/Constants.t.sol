8a03d8303999a0b85f331ac0228d09cc9cec1626
Jay White <JayWhite2357@users.noreply.github.com>
2025-02-01 21:44:15 -0500

    feat(sol): add sumcheck verifier

--- solidity/test/base/Constants.t.sol
+++ solidity/test/base/Constants.t.sol
@@ -5,8 +5,14 @@ pragma solidity ^0.8.28;
 import {Test} from "forge-std/Test.sol";
 
 import {Errors} from "../../src/base/Constants.sol";
-// solhint-disable-next-line no-unused-import
-import {INVALID_EC_ADD_INPUTS, INVALID_EC_MUL_INPUTS, INVALID_EC_PAIRING_INPUTS} from "../../src/base/Constants.sol";
+/* solhint-disable no-unused-import */
+import {
+    INVALID_EC_ADD_INPUTS,
+    INVALID_EC_MUL_INPUTS,
+    INVALID_EC_PAIRING_INPUTS,
+    ROUND_EVALUATION_MISMATCH
+} from "../../src/base/Constants.sol";
+/* solhint-enable no-unused-import */
 
 contract ConstantsTest is Test {
     function testErrorFailedInvalidECAddInputs() public {
@@ -29,6 +35,14 @@ contract ConstantsTest is Test {
         vm.expectPartialRevert(Errors.InvalidECPairingInputs.selector);
         assembly {
             mstore(0, INVALID_EC_PAIRING_INPUTS)
+            revert(0, 4)
+        }
+    }
+
+    function testErrorFailedRoundEvaluationMismatch() public {
+        vm.expectPartialRevert(Errors.RoundEvaluationMismatch.selector);
+        assembly {
+            mstore(0, ROUND_EVALUATION_MISMATCH)
             revert(0, 4)
         }
     }
